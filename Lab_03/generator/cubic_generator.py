c1a88030a8a950fccc7db2da4eb07c394c416af9
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-18 18:52:26 +0100

    Done task 08.

---
+++ Lab_03/generator/cubic_generator.py
@@ -0,0 +1,13 @@
+from Lab_03.generator.square_generator import SquareGenerator, InvalidRangeException
+
+
+class CubicGenerator(SquareGenerator):
+    def __init__(self, start, end):
+        super().__init__(start, end)
+
+    def cubes_from_range(self):
+        if self.end < self.start:
+            raise InvalidRangeException("End of the range was less than start parameter.")
+        return [i*i*i for i in range(self.start, self.end)]
+
+
